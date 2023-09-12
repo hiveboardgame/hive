86e9c5dd7afc72423982bfb2bbe58ea95f1da94c
ioni <104294646+IongIer@users.noreply.github.com>
2023-09-12 19:12:54 +0300

    Adds missing header in play page

--- app/src/pages/play.rs
+++ app/src/pages/play.rs
@@ -8,7 +8,7 @@ pub fn PlayPage(cx: Scope) -> impl IntoView {
     // provide_context(cx, create_rw_signal(cx, GameState::new(cx)));
 
     view! { cx,
-
+        <Header/>
         <div class="row" style="width: 99vw; height: 89vh; display:flex;">
                 <Reserve color=Color::White/>
                 <Board />
