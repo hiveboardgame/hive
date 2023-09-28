e68edf53a281fe0cae5e51bea2cef5d3dac4765d
ioni <104294646+IongIer@users.noreply.github.com>
2023-09-28 14:28:01 +0300

    Implements zoom on pointer with mouse/touchpad

--- app/src/organisms/board.rs
+++ app/src/organisms/board.rs
@@ -5,12 +5,13 @@ use crate::common::{
 };
 use crate::molecules::{board_pieces::BoardPieces, history_pieces::HistoryPieces};
 use hive_lib::position::Position;
-use leptos::ev::{contextmenu, pointerdown, pointerleave, pointermove, pointerup};
+use leptos::ev::{contextmenu, pointerdown, pointerleave, pointermove, pointerup, wheel};
+use leptos::logging::*;
 use leptos::svg::Svg;
 use leptos::*;
 use leptos_use::use_event_listener;
 use wasm_bindgen::JsCast;
-use web_sys::PointerEvent;
+use web_sys::{PointerEvent, WheelEvent};
 
 #[derive(Debug, Clone)]
 struct ViewBoxControls {
@@ -51,6 +52,7 @@ pub fn Board(
     let viewbox_signal = create_rw_signal(ViewBoxControls::new());
     let viewbox_ref = create_node_ref::<svg::Svg>();
     let div_ref = create_node_ref::<html::Div>();
+
     let viewbox_string = move || {
         format!(
             "{} {} {} {}",
@@ -71,8 +73,10 @@ pub fn Board(
 
     let initial_position = Position::initial_spawn_position();
     let svg_pos = SvgPos::center_for_level(initial_position, 0);
+
     div_ref.on_load(move |_| {
         create_effect(move |_| {
+            //Make initial piece spawn in center of board
             let div = div_ref.get_untracked().expect("Div should already exist");
             viewbox_signal.update(|viewbox_controls: &mut ViewBoxControls| {
                 viewbox_controls.width = div.offset_width() as f32;
@@ -81,18 +85,20 @@ pub fn Board(
                 viewbox_controls.y_transform = -(svg_pos.1 - (div.offset_height() as f32 / 2.0));
             });
 
+            //Start panning and record point where it starts for mouse and touch
             _ = use_event_listener(viewbox_ref, pointerdown, move |evt| {
                 is_panning.update_untracked(|b| *b = true);
-                let ref_point = svg_point_from_event(viewbox_ref, evt);
+                let ref_point = svg_point_from_pointer(viewbox_ref, &evt);
                 viewbox_signal.update(|viewbox_controls: &mut ViewBoxControls| {
                     viewbox_controls.drag_start_x = ref_point.x();
                     viewbox_controls.drag_start_y = ref_point.y();
                 });
             });
 
+            //Keep panning while user drags around
             _ = use_event_listener(viewbox_ref, pointermove, move |evt| {
                 if is_panning.get_untracked() {
-                    let moved_point = svg_point_from_event(viewbox_ref, evt);
+                    let moved_point = svg_point_from_pointer(viewbox_ref, &evt);
                     viewbox_signal.update(|viewbox_controls: &mut ViewBoxControls| {
                         viewbox_controls.x -= moved_point.x() - viewbox_controls.drag_start_x;
                         viewbox_controls.y -= moved_point.y() - viewbox_controls.drag_start_y;
@@ -100,14 +106,38 @@ pub fn Board(
                 }
             });
 
+            //Zoom on point with mousewheel/touchpad
+            _ = use_event_listener(viewbox_ref, wheel, move |evt| {
+                evt.prevent_default();
+                if !is_panning.get_untracked() {
+                    let initial_point = svg_point_from_wheel(viewbox_ref, &evt);
+                    let scale: f32 = if evt.delta_y() > 0.0 { 0.09 } else { -0.09 };
+                    viewbox_signal.update(|viewbox_controls: &mut ViewBoxControls| {
+                        let initial_width = viewbox_controls.width;
+                        let initial_height = viewbox_controls.height;
+                        viewbox_controls.width += initial_width * scale;
+                        viewbox_controls.height += initial_height * scale;
+                        viewbox_controls.x = initial_point.x()
+                            - (initial_point.x() - viewbox_controls.x) / initial_width
+                                * viewbox_controls.width;
+                        viewbox_controls.y = initial_point.y()
+                            - (initial_point.y() - viewbox_controls.y) / initial_height
+                                * viewbox_controls.height;
+                    });
+                }
+            });
+
+            //Stop panning when user releases touch/click
             _ = use_event_listener(viewbox_ref, pointerup, move |_| {
                 is_panning.update_untracked(|b| *b = false);
             });
 
+            //Stop panning when pointer leaves board area
             _ = use_event_listener(viewbox_ref, pointerleave, move |_| {
                 is_panning.update_untracked(|b| *b = false);
             });
 
+            //Prevent right click/context menu on board
             _ = use_event_listener(viewbox_ref, contextmenu, move |evt| {
                 evt.prevent_default();
             });
@@ -153,13 +183,21 @@ pub fn Board(
     }
 }
 
-fn svg_point_from_event(svg: NodeRef<Svg>, evt: PointerEvent) -> web_sys::SvgPoint {
+fn svg_point_from_pointer(svg: NodeRef<Svg>, evt: &PointerEvent) -> web_sys::SvgPoint {
+    svg_point_from_coordinates(svg, evt.x() as f32, evt.y() as f32)
+}
+
+fn svg_point_from_wheel(svg: NodeRef<Svg>, evt: &WheelEvent) -> web_sys::SvgPoint {
+    svg_point_from_coordinates(svg, evt.x() as f32, evt.y() as f32)
+}
+
+fn svg_point_from_coordinates(svg: NodeRef<Svg>, x: f32, y: f32) -> web_sys::SvgPoint {
     let svg = svg.get_untracked().expect("svg should exist already");
     let svg_graphics_element = svg.unchecked_ref::<web_sys::SvgGraphicsElement>();
     let svg_svg_element = svg.unchecked_ref::<web_sys::SvgsvgElement>();
     let point: web_sys::SvgPoint = svg_svg_element.create_svg_point();
-    point.set_x(evt.client_x() as f32);
-    point.set_y(evt.client_y() as f32);
+    point.set_x(x);
+    point.set_y(y);
 
     point.matrix_transform(
         &svg_graphics_element
