de1c1f22a04c5fb0b39780feefe6c6f82ac9b27f
Felix Glaser <leex@klautcomputing.de>
2024-01-13 18:43:57 +0100

    Reconnect after 10s offline

--- apis/src/components/organisms/header.rs
+++ apis/src/components/organisms/header.rs
@@ -3,6 +3,7 @@ use crate::components::organisms::{
     darkmode_toggle::DarkModeToggle, hamburger::Hamburger, logout::Logout,
 };
 use crate::providers::auth_context::*;
+use crate::providers::web_socket::WebsocketContext;
 use leptos::*;
 use leptos_router::use_location;
 
@@ -14,6 +15,8 @@ pub fn Header(#[prop(optional)] extend_tw_classes: &'static str) -> impl IntoVie
     let auth_context = expect_context::<AuthContext>();
     let hamburger_show = create_rw_signal(false);
     let onclick = move || hamburger_show.update(|b| *b = false);
+    let websocket = expect_context::<WebsocketContext>();
+    let ws_state = move || websocket.ready_state.get().to_string();
     view! {
         <header class=format!(
             "h-8 md:h-10 lg:h-12 w-full fixed top-0 flex justify-between items-center bg-gray-300 dark:bg-gray-700 z-50 max-w-[100vw] {extend_tw_classes}",
@@ -21,6 +24,8 @@ pub fn Header(#[prop(optional)] extend_tw_classes: &'static str) -> impl IntoVie
             <a class="ml-10" href="/">
                 Home
             </a>
+
+            { ws_state }
             <Transition>
                 {move || {
                     let user = move || match (auth_context.user)() {
