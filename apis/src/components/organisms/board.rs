39ea322a7ff3763c3c3d77cc43c924969b041326
ioni <104294646+IongIer@users.noreply.github.com>
2024-03-01 10:15:23 +0200

    Fixes windows stack collapse

--- apis/src/components/organisms/board.rs
+++ apis/src/components/organisms/board.rs
@@ -274,10 +274,9 @@ pub fn Board(
         UseEventListenerOptions::default().passive(true),
     );
 
-    //Stop panning when user releases touch/click AND reset height adjustment on right click release
+    //Stop panning when user releases touch/click
     _ = use_event_listener(viewbox_ref, pointerup, move |_| {
         is_panning.update_untracked(|b| *b = false);
-        target_stack.set(None);
     });
 
     //Stop panning when pointer leaves board area
