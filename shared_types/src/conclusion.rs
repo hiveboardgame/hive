06127a11be4a51c679c2ed78b50e0408a4f35baf
Felix Glaser <leex@klautcomputing.de>
2024-03-27 14:00:28 +0100

    Shows game conclusions and repetitions in history

---
+++ shared_types/src/conclusion.rs
@@ -0,0 +1,51 @@
+use serde::{Deserialize, Serialize};
+use std::fmt;
+
+#[derive(Serialize, Deserialize, PartialEq, Eq, Debug, Clone)]
+pub enum Conclusion {
+    Unknown,
+    Resigned,
+    Timeout,
+    Draw,
+    Board,
+    Repetition,
+}
+
+impl fmt::Display for Conclusion {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let time = match self {
+            Conclusion::Unknown => "Unknown",
+            Conclusion::Resigned => "Resigned",
+            Conclusion::Timeout => "Timeout",
+            Conclusion::Draw => "Draw",
+            Conclusion::Board => "Board",
+            Conclusion::Repetition => "Repetition",
+        };
+        write!(f, "{}", time)
+    }
+}
+
+use thiserror::Error;
+#[derive(Error, Debug, Clone, Serialize, Deserialize)]
+pub enum ConclusionError {
+    #[error("{found} is not a valid Conclusion")]
+    InvalidConclusion { found: String },
+}
+
+impl std::str::FromStr for Conclusion {
+    type Err = ConclusionError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Unknown" => Ok(Conclusion::Unknown),
+            "Resigned" => Ok(Conclusion::Resigned),
+            "Timeout" => Ok(Conclusion::Timeout),
+            "Draw" => Ok(Conclusion::Draw),
+            "Board" => Ok(Conclusion::Board),
+            "Repetition" => Ok(Conclusion::Repetition),
+            s => Err(ConclusionError::InvalidConclusion {
+                found: s.to_string(),
+            }),
+        }
+    }
+}
