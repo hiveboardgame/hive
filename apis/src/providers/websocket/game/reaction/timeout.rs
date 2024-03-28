0b224e3265091c0da595c1cceca9c837c7cdc06c
ioni <104294646+IongIer@users.noreply.github.com>
2024-03-28 12:07:13 +0200

    Fixes timeouts not triggering gamend changes and sticky history view

--- apis/src/providers/websocket/game/reaction/timeout.rs
+++ apis/src/providers/websocket/game/reaction/timeout.rs
@@ -1,9 +1,12 @@
-use crate::providers::games::GamesSignal;
-
+use crate::common::server_result::GameActionResponse;
+use crate::providers::{game_state::GameStateSignal, games::GamesSignal};
 use leptos::*;
 
-pub fn handle_timeout(nanoid: String) {
+pub fn handle_timeout(gar: GameActionResponse) {
     let mut games = expect_context::<GamesSignal>();
-    games.own_games_remove(&nanoid);
-    games.live_games_remove(&nanoid);
+    let mut game_state = expect_context::<GameStateSignal>();
+    let nanoid = &gar.game.nanoid;
+    games.own_games_remove(nanoid);
+    games.live_games_remove(nanoid);
+    game_state.set_game_response(gar.game.clone());
 }
