5de51051834b1439dff21dc980bbd32ca1f2b304
Felix Glaser <leex@klautcomputing.de>
2024-04-15 11:21:53 +0200

    Shows unplayed games in title

---
+++ apis/src/components/atoms/title.rs
@@ -0,0 +1,21 @@
+use crate::providers::{games::GamesSignal, refocus::RefocusSignal};
+use leptos::*;
+use leptos_meta::Title as T;
+
+#[component]
+pub fn Title() -> impl IntoView {
+    let games = expect_context::<GamesSignal>();
+    let focused = expect_context::<RefocusSignal>();
+    let title_text = move || {
+        let len = games.own.get().next_untimed.len()
+            + games.own.get().next_realtime.len()
+            + games.own.get().next_correspondence.len();
+        if !focused.signal.get().focused && len > 0 {
+            format!("({}) HiveGame.com", len)
+        } else {
+            String::from("HiveGame.com")
+        }
+    };
+
+    view! { <T text=title_text/> }
+}
