2b3289eb2b257df9c9a462d58a375eebe383d1f3
Penguinwithatie <Penguinwithatie@protonmail.com>
2024-05-18 19:33:10 -0600

    add "quickplay" feature

---
+++ apis/src/components/organisms/quickplay.rs
@@ -0,0 +1,120 @@
+use crate::{
+    common::ChallengeAction,
+    components::{atoms::rating::icon_for_speed, molecules::modal::Modal},
+    pages::challenge_create::ChallengeCreate,
+    providers::{ApiRequests, AuthContext},
+};
+use core::panic;
+use hive_lib::{ColorChoice, GameType};
+use leptos::{html::Dialog, *};
+use leptos_icons::*;
+use leptos_router::use_navigate;
+use shared_types::ChallengeVisibility;
+use shared_types::{ChallengeDetails, GameSpeed::*, TimeMode};
+pub enum QuickPlayTimeControl {
+    Bullet1p2,
+    Blitz3p3,
+    Blitz5p4,
+    Rapid10p10,
+    Rapid15p10,
+    Classic20p20,
+    Classic30p30,
+}
+use QuickPlayTimeControl::*;
+const BUTTON_STYLE: &str = "flex justify-center px-4 py-2 font-bold text-white rounded bg-button-dawn dark:bg-button-twilight hover:bg-pillbug-teal active:scale-95";
+
+#[component]
+pub fn GridButton(time_control: QuickPlayTimeControl) -> impl IntoView {
+    let (display_text, icon_data, base, increment) = match time_control {
+        Bullet1p2 => ("1+2".to_owned(), icon_for_speed(&Bullet), 1, 2),
+        Blitz3p3 => ("3+3".to_owned(), icon_for_speed(&Blitz), 3, 3),
+        Blitz5p4 => ("5+4".to_owned(), icon_for_speed(&Blitz), 5, 4),
+        Rapid10p10 => ("10+10".to_owned(), icon_for_speed(&Rapid), 10, 10),
+        Rapid15p10 => ("15+10".to_owned(), icon_for_speed(&Rapid), 15, 10),
+        Classic20p20 => ("20+20".to_owned(), icon_for_speed(&Classic), 20, 20),
+        Classic30p30 => ("30+30".to_owned(), icon_for_speed(&Classic), 30, 30),
+    };
+    view! {
+        <button
+            class=BUTTON_STYLE
+            on:click=move |_| {
+                let auth_context = expect_context::<AuthContext>();
+                let account = match (auth_context.user)() {
+                    Some(Ok(Some(account))) => Some(account),
+                    _ => None,
+                };
+                if account.is_some() {
+                    let api = ApiRequests::new();
+                    let details = ChallengeDetails {
+                        rated: true,
+                        game_type: GameType::MLP,
+                        visibility: ChallengeVisibility::Public,
+                        opponent: None,
+                        color_choice: ColorChoice::Random,
+                        time_mode: TimeMode::RealTime,
+                        time_base: Some(base * 60),
+                        time_increment: Some(increment),
+                        band_upper: None,
+                        band_lower: None,
+                    };
+                    let challenge_action = ChallengeAction::Create(details);
+                    api.challenge(challenge_action);
+                } else {
+                    let navigate = use_navigate();
+                    navigate("/login", Default::default());
+                }
+            }
+        >
+
+            <Icon icon=icon_data/>
+            {display_text}
+        </button>
+    }
+}
+#[component]
+pub fn QuickPlay() -> impl IntoView {
+    let open = create_rw_signal(false);
+    let dialog_el = create_node_ref::<Dialog>();
+    let close_modal = Callback::new(move |()| {
+        dialog_el
+            .get_untracked()
+            .expect("dialog to have been created")
+            .close();
+    });
+    view! {
+        <div>
+            <Modal open=open dialog_el=dialog_el>
+                <ChallengeCreate close=close_modal/>
+            </Modal>
+            <span class="flex justify-center mb-4 text-xl font-bold">Quick Play</span>
+            <div class="grid grid-cols-2 gap-4 w-full md:grid-cols-3">
+                <GridButton time_control=Bullet1p2/>
+                <GridButton time_control=Blitz3p3/>
+                <GridButton time_control=Blitz5p4/>
+                <GridButton time_control=Rapid10p10/>
+                <GridButton time_control=Rapid15p10/>
+                <GridButton time_control=Classic20p20/>
+                <GridButton time_control=Classic30p30/>
+                <button
+                    class=BUTTON_STYLE
+                    on:click=move |_| {
+                        let auth_context = expect_context::<AuthContext>();
+                        let account = match (auth_context.user)() {
+                            Some(Ok(Some(account))) => Some(account),
+                            _ => None,
+                        };
+                        if account.is_some() {
+                            open.update(move |b| *b = true)
+                        } else {
+                            let navigate = use_navigate();
+                            navigate("/login", Default::default());
+                        }
+                    }
+                >
+
+                    Custom
+                </button>
+            </div>
+        </div>
+    }
+}
