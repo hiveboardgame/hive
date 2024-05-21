8c9d2286cdb5b92171a9fc4093451f66c1545d25
Penguinwithatie <Penguinwithatie@protonmail.com>
2024-05-21 09:30:35 -0600

    active search on online players

--- apis/src/components/organisms/online_users.rs
+++ apis/src/components/organisms/online_users.rs
@@ -6,17 +6,42 @@ use crate::{components::molecules::user_row::UserRow, providers::online_users::O
 pub fn OnlineUsers() -> impl IntoView {
     let online_users = expect_context::<OnlineUsersSignal>();
     let online_players = move || (online_users.signal)().username_user;
-    let is_empty = move || online_players().is_empty();
+    let total_online = move || online_players().len();
+    let search = create_rw_signal("".to_string());
     view! {
         <div class="flex flex-col pt-10 m-2 w-fit">
-            Online players: <div class="overflow-y-auto max-h-96 h-fit">
-                <div class=move || {
-                    format!("p-1 h-6 {}", if !is_empty() { "hidden" } else { "flex" })
-                }>{move || if is_empty() { "Only you" } else { "" }}</div>
+            <input
+                type="text"
+                on:input=move |ev| {
+                    search.set(event_target_value(&ev));
+                }
 
-                <For each=online_players key=|(key, _)| key.to_owned() let:a_user>
-                    <UserRow user=store_value(a_user.1)/>
-                </For>
+                placeholder="Search for a player"
+                prop:value=search
+            />
+
+            {total_online}
+            Online players:
+            <div class="overflow-y-auto max-h-96 h-fit">
+                <div class=move || {
+                    format!("p-1 h-6 {}", if total_online() > 0 { "hidden" } else { "flex" })
+                }>{move || if total_online() == 0 { "Only you" } else { "" }}</div>
+
+                <For
+                    each=online_players
+                    key=move |(key, _)| (key.to_owned(), search())
+                    let:a_user
+                    children=move |a_user| {
+                        if search().is_empty()
+                            || a_user.1.username.to_lowercase().contains(&search().to_lowercase())
+                        {
+                            view! { <UserRow user=store_value(a_user.1)/> }
+                        } else {
+                            "".into_view()
+                        }
+                    }
+                />
+
             </div>
         </div>
     }
