634a0f783595c32c5a3e53221cbbfde6fbd533f5
ioni <104294646+IongIer@users.noreply.github.com>
2024-06-20 16:18:04 +0300

    Adds content to tournament invitation notification row

--- apis/src/components/molecules/tournament_invitation_row.rs
+++ apis/src/components/molecules/tournament_invitation_row.rs
@@ -1,43 +1,66 @@
-use crate::components::atoms::status_indicator::StatusIndicator;
+use crate::common::TournamentAction;
 use crate::components::molecules::time_row::TimeRow;
 use crate::providers::ApiRequests;
-use crate::{
-    components::atoms::game_type::GameType,
-    components::atoms::profile_link::ProfileLink,
-    functions::hostname::hostname_and_port,
-    providers::{game_state::GameStateSignal, AuthContext, ColorScheme},
-    responses::TournamentResponse,
-};
+use crate::responses::TournamentResponse;
 use leptos::*;
 use leptos_icons::*;
-use leptos_router::*;
-use leptos_use::use_window;
-use shared_types::ChallengeVisibility;
 
 #[component]
 pub fn TournamentInvitationRow(tournament: StoredValue<TournamentResponse>) -> impl IntoView {
-    let auth_context = expect_context::<AuthContext>();
+    let div_class = "xs:py-1 xs:px-1 sm:py-2 sm:px-2";
+    let seats_taken = format!("{}/{}", tournament().players.len(), tournament().seats);
 
-    let tournament_address = move || {
-        format!(
-            "{}/tournaments/{}",
-            hostname_and_port(),
-            tournament().tournament_id
-        )
+    let decline = move |_| {
+        let api = ApiRequests::new();
+        api.tournament(TournamentAction::InvitationDecline(
+            tournament().tournament_id,
+        ));
     };
-
-    let td_class = "xs:py-1 xs:px-1 sm:py-2 sm:px-2";
-    let uid = move || match (auth_context.user)() {
-        Some(Ok(Some(user))) => Some(user.id),
-        _ => None,
+    let accept = move |_| {
+        let api = ApiRequests::new();
+        api.tournament(TournamentAction::InvitationAccept(
+            tournament().tournament_id,
+        ));
     };
 
     view! {
-        <div> Tournament row</div>
-        <tr class="items-center text-center cursor-pointer dark:odd:bg-header-twilight dark:even:bg-reserve-twilight odd:bg-odd-light even:bg-even-light max-w-fit">
-            <td class=td_class>
-                <div>Tournament</div>
-            </td>
-        </tr>
+        <div class="flex items-center text-center cursor-pointer dark:odd:bg-header-twilight dark:even:bg-reserve-twilight odd:bg-odd-light even:bg-even-light max-w-fit">
+            <div class="flex relative">
+                <div class=div_class>
+                    <div>{tournament().name}</div>
+                </div>
+                <div class=div_class>
+                    <TimeRow
+                        time_mode=tournament().time_mode
+                        time_base=tournament().time_base
+                        increment=tournament().time_increment
+                    />
+
+                </div>
+                <div class=div_class>
+                    <div>Players: {seats_taken}</div>
+                </div>
+                <a
+                    class="absolute top-0 left-0 z-10 w-full h-full"
+                    href=format!("/tournament/{}", tournament().tournament_id)
+                ></a>
+            </div>
+            <div class=div_class>
+                <button
+                    title="Accept Invitation"
+                    on:click=accept
+                    class="z-20 p-1 mx-2 text-white rounded transition-transform duration-300 transform bg-button-dawn dark:bg-button-twilight hover:bg-pillbug-teal active:scale-95"
+                >
+                    <Icon icon=icondata::AiCheckOutlined class="w-6 h-6"/>
+                </button>
+                <button
+                    title="Decline Invitation"
+                    on:click=decline
+                    class="z-20 p-1 mx-2 text-white rounded transition-transform duration-300 transform bg-ladybug-red hover:bg-red-400 active:scale-95"
+                >
+                    <Icon icon=icondata::IoCloseSharp class="w-6 h-6"/>
+                </button>
+            </div>
+        </div>
     }
 }
