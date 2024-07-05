f56d164c828a4212bf91de992ce81ee6fc35f28d
Penguinwithatie <Penguinwithatie@protonmail.com>
2024-07-04 18:35:21 -0600

    add search test

--- apis/end2end/tests/example.spec.ts
+++ apis/end2end/tests/example.spec.ts
@@ -1,19 +1,15 @@
 import { test, expect } from "@playwright/test";
 
-test("Invalid login", async ({ page, browser }) => {
+test("Positive and negative login test", async ({ page, browser }) => {
   await page.goto("/login");
-
+  //Negative
   await page.getByPlaceholder("E-mail").fill("TestUser1Email@example.com");
   await page.getByLabel("Password").fill("Wrong Password!");
   await page.getByRole("button", { name: "Sign In" }).click();
 
   await expect(page).toHaveURL("/login");
   await expect(page.getByText(/Invalid email or password/)).toBeVisible();
-});
-
-
-test("Correct login and online indicator", async ({ page, browser }) => {
-  await page.goto("/login");
+  //Positive
 
   await page.getByPlaceholder("E-mail").fill("TestUser1Email@example.com"); // TODO: change to another user
   await page.getByLabel("Password").fill("12345678");
@@ -21,4 +17,13 @@ test("Correct login and online indicator", async ({ page, browser }) => {
 
   await expect(page).toHaveURL("/");
   await expect(page.getByText(/online player/)).toContainText("TestUser1");
+});
+
+test('Search user', async ({ page }) => {
+  page.on('websocket', ws => {
+    console.log(`WebSocket opened: ${ws.url()}>`);
+  });
+  await page.goto('/');
+  await page.getByPlaceholder('Search players').fill('TestUser1');
+  await expect(page.getByRole('link', { name: 'TestUser1' })).toBeVisible();
 });
