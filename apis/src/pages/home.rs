609cc556a94ccfca479b8c4137b46faecf50e0e2
leex <leex@klautcomputing.de>
2024-07-15 09:45:03 +0300

    Removes RL banner

--- apis/src/pages/home.rs
+++ apis/src/pages/home.rs
@@ -1,4 +1,3 @@
-use crate::components::molecules::rl_banner::RlBanner;
 use crate::components::organisms::{
     challenges::Challenges, online_users::OnlineUsers, quickplay::QuickPlay, tv::Tv,
 };
@@ -8,7 +7,6 @@ use leptos::*;
 pub fn Home() -> impl IntoView {
     view! {
         <div class="flex overflow-x-hidden flex-col justify-start items-center pt-20 w-full md:justify-center">
-            <RlBanner/>
             <div class="flex flex-col justify-center items-center lg:flex-row lg:items-start">
                 <div class="flex flex-col items-center w-full md:flex-row">
                     <div class="flex flex-col items-center">
