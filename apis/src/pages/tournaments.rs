67131255c7730cedae46fadd1ddb50fb2f7a23c9
PenguinWithATie <166940857+PenguinWithATie@users.noreply.github.com>
2024-09-23 09:24:32 -0600

    simple tournament search by name (#333)

--- apis/src/pages/tournaments.rs
+++ apis/src/pages/tournaments.rs
@@ -17,17 +17,34 @@ pub fn Tournaments() -> impl IntoView {
             api.tournament(TournamentAction::GetAll(Abstract));
         };
     });
+    let search = RwSignal::new("".to_string());
     view! {
         <div class="pt-10">
             <div class="container px-4 mx-auto">
-                Tournaments
+                <input
+                    type="text"
+                    class="items-center p-2 mx-2 my-2 w-5/6"
+                    placeholder="Search by tournament name or description"
+                    on:input=move |ev| search.set(event_target_value(&ev))
+                    value=search
+                />
                 <For
                     each=move || { tournament.summary.get().tournaments }
-                    key=|(nanoid, tournament)| { (nanoid.to_owned(), tournament.updated_at) }
+                    key=move |(nanoid, tournament)| {
+                        (nanoid.to_owned(), tournament.updated_at, search())
+                    }
                     let:tournament
-                >
-                    <TournamentRow tournament=tournament.1/>
-                </For>
+                    children=move |tournament| {
+                        if search().is_empty()
+                            || tournament.1.name.to_lowercase().contains(&search().to_lowercase())
+                        {
+                            view! { <TournamentRow tournament=tournament.1/> }
+                        } else {
+                            "".into_view()
+                        }
+                    }
+                />
+
             </div>
         </div>
     }
