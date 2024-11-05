19a72403ca333d327a3b5772ed380f6681bb3406
PenguinWithATie <166940857+PenguinWithATie@users.noreply.github.com>
2024-11-05 10:38:40 -0600

    sync_to_main (#361)

---
+++ shared_types/src/takeback_conf.rs
@@ -0,0 +1,29 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Default,Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+pub enum Takeback {
+    #[default]
+    Always,
+    Never,
+    CasualOnly,
+}
+
+impl Takeback {
+    pub fn from_str_or_default(s: &str) -> Self {
+        match s {
+            "always" => Self::Always,
+            "never" => Self::Never,
+            "casual" => Self::CasualOnly,
+            _ => Self::default(),
+        }
+    }
+}
+impl std::fmt::Display for Takeback {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Always => write!(f, "always"),
+            Self::Never => write!(f, "never"),
+            Self::CasualOnly => write!(f, "casual"),
+        }
+    }
+}
