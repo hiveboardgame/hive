28d2b13cf36381607adec148425516a346ef793e
leex <leex@klautcomputing.de>
2024-12-05 15:20:11 +0100

    Adds Fair Play Cup notification

--- apis/src/components/molecules/rl_banner.rs
+++ apis/src/components/molecules/rl_banner.rs
@@ -5,18 +5,25 @@ pub fn RlBanner() -> impl IntoView {
     view! {
         <div class="flex flex-col justify-center items-center p-6 mb-3 text-black rounded-sm bg-orange-twilight xs:p-8 xs:mb-4">
             <h1 class="flex items-center mb-4 text-2xl font-bold xs:text-4xl">
-                Rapid League season starts Nov 12!
+                Fair Play Cup 2025 - Team Tournament
             </h1>
             <div class="flex flex-col">
                 <a
-                    href="https://docs.google.com/forms/d/e/1FAIpQLSeF5VFfWy2Tfj4KLfn02kRwvlgaP78VH0wWs6kp0yW5adYRxQ/viewform"
+                    href="https://www.worldhivetournaments.com/hive-fair-play-cup/"
                     rel="external"
                     target="_blank"
                     class="place-self-center text-blue-500 hover:underline"
                 >
-                    Sign up before Nov 12
+                    Sign up by January 4th 2025
                 </a>
-                Flexible schedule of 5-6 matches over 10 weeks with a 10 + 10 time control.
+                <ul>
+                    <li> Starts January 6th </li>
+                    <li>
+                        Each team consists of 3 to 4 players (only 3 players per team play each round)
+                    </li>
+                    <li>One match (2 games) every 2 weeks</li>
+                    <li>Time control: 15 min + 15 sec</li>
+                </ul>
                 <div>
                     Schedule your games here or over
                     <a
