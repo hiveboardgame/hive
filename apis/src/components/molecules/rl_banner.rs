33b20ffe3df07f9933437996f53fde71411a63ba
leex <leex@klautcomputing.de>
2025-01-06 13:04:05 +0100

    Adds KoTH Feb banner

--- apis/src/components/molecules/rl_banner.rs
+++ apis/src/components/molecules/rl_banner.rs
@@ -5,37 +5,30 @@ pub fn RlBanner() -> impl IntoView {
     view! {
         <div class="flex flex-col justify-center items-center p-6 mb-3 text-black rounded-sm bg-orange-twilight xs:p-8 xs:mb-4">
             <h1 class="flex items-center mb-4 text-2xl font-bold xs:text-4xl">
-                Fair Play Cup 2025 - Team Tournament
+                King of the Hive 
             </h1>
-            <div class="flex flex-col">
+            <p>
+                2025 February Season starts soon! Find out more 
                 <a
-                    href="https://www.worldhivetournaments.com/hive-fair-play-cup/"
+                    href="https://koth.fly.dev/"
                     rel="external"
                     target="_blank"
-                    class="place-self-center text-blue-500 hover:underline"
+                    class="place-self-center text-blue-500 hover:underline inline"
                 >
-                    Sign up by January 4th 2025
+                    here!
                 </a>
-                <ul>
-                    <li>Starts January 6th</li>
-                    <li>
-                        Each team consists of 3 to 4 players (only 3 players per team play each round)
-                    </li>
-                    <li>One match (2 games) every 2 weeks</li>
-                    <li>Time control: 15 min + 15 sec</li>
-                </ul>
-                <div>
-                    Schedule your games here or over
-                    <a
-                        href="https://discord.gg/YwDEmYPHrZ"
-                        rel="external"
-                        target="_blank"
-                        class="text-blue-500 hover:underline"
-                    >
-                        Discord
-                    </a>
-                </div>
-            </div>
+            </p>
+            <p>
+                <a
+                    href="https://koth.fly.dev/register"
+                    rel="external"
+                    target="_blank"
+                    class="place-self-center text-blue-500 hover:underline inline"
+                >
+                    Sign up
+                </a>
+                by January 19th 2025
+            </p>
         </div>
     }
 }
