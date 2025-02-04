6d104f9ac488e414fc11d479a9fcd2c7c5d9b511
leex <leex@klautcomputing.de>
2025-02-04 11:12:59 +0100

    Adds rapid league banner

--- apis/src/components/molecules/rl_banner.rs
+++ apis/src/components/molecules/rl_banner.rs
@@ -4,28 +4,21 @@ use leptos::*;
 pub fn RlBanner() -> impl IntoView {
     view! {
         <div class="flex flex-col justify-center items-center p-6 mb-3 text-black rounded-sm bg-orange-twilight xs:p-8 xs:mb-4">
-            <h1 class="flex items-center mb-4 text-2xl font-bold xs:text-4xl">King of the Hive</h1>
-            <p>
-                2025 February Season starts soon! Find out more
-                <a
-                    href="https://koth.fly.dev/"
-                    rel="external"
-                    target="_blank"
-                    class="place-self-center text-blue-500 hover:underline inline"
-                >
-                    here!
-                </a>
-            </p>
+            <h1 class="flex items-center mb-4 text-2xl font-bold xs:text-4xl">Hive Rapid League</h1>
+            <p>Season 7 starts soon!</p>
+            <p>Play against opponents at a similar level</p>
+            <p>10 weeks, 1 game per week, flexible scheduling</p>
+            <p>Time control is 10 min + 10 sec</p>
             <p>
                 <a
-                    href="https://koth.fly.dev/register"
+                    href="https://forms.gle/kiAHxJoxQw5DJQvw7"
                     rel="external"
                     target="_blank"
                     class="place-self-center text-blue-500 hover:underline inline"
                 >
                     Sign up
                 </a>
-                by January 19th 2025
+                by 15th February
             </p>
         </div>
     }
