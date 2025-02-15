6e9e89ccfef096cd35f71013d02cd5e027eaf29e
Penguinwithatie <Penguinwithatie@protonmail.com>
2025-02-15 13:28:55 -0600

    better authctx

--- apis/src/websocket/client_handlers/server_user_conf.rs
+++ apis/src/websocket/client_handlers/server_user_conf.rs
@@ -1,8 +1,18 @@
-use crate::providers::AuthContext;
-use leptos::prelude::expect_context;
+use crate::{functions::accounts::get::get_account, providers::{websocket::WebsocketContext, AuthContext}};
+use leptos::{prelude::{expect_context, Set}, task::spawn};
 pub fn handle_server_user_conf(success: bool) {
     let auth_context = expect_context::<AuthContext>();
     if success {
-        auth_context.user.refetch();
+        spawn(async move {
+            let account = get_account().await;
+            if let Ok(account) = account {
+                auth_context.user.set(Some(Ok(account)));
+                let websocket_context = expect_context::<WebsocketContext>();
+                websocket_context.close();
+                websocket_context.open();
+            } else {
+                auth_context.user.set(None);
+            }
+        });
     }
 }
