6fb302af0d86d0d9baf0c1145eea971b65aac572
leex <leex@klautcomputing.de>
2025-02-18 13:53:32 +0100

    Nicer link

--- apis/src/websocket/server_handlers/game/turn_handler.rs
+++ apis/src/websocket/server_handlers/game/turn_handler.rs
@@ -93,7 +93,7 @@ impl TurnHandler {
             _ => {
                 let url = format!("http://localhost:8080/msg/{}", game.current_player_id);
                 let mut json = HashMap::new();
-                let msg = format!("It's your turn: https://hivegame.com/game/{}", game.nanoid);
+                let msg = format!("[It's your turn!](<https://hivegame.com/game/{}>)", game.nanoid);
                 json.insert("content", msg);
                 let client = reqwest::Client::new();
                 let res = client.post(url).json(&json).send().await?;
