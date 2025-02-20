bd22fa684a60dcd57f494afbc49a9e8ef1ef5889
Penguinwithatie <Penguinwithatie@protonmail.com>
2025-02-19 21:44:26 -0600

    use action for login stuff

--- apis/src/websocket/client_handlers/server_user_conf.rs
+++ apis/src/websocket/client_handlers/server_user_conf.rs
@@ -1,18 +1,8 @@
-use crate::{functions::accounts::get::get_account, providers::{websocket::WebsocketContext, AuthContext}};
-use leptos::{prelude::{expect_context, Set}, task::spawn};
+use crate::providers::AuthContext;
+use leptos::prelude::expect_context;
 pub fn handle_server_user_conf(success: bool) {
     let auth_context = expect_context::<AuthContext>();
     if success {
-        spawn(async move {
-            let account = get_account().await;
-            if let Ok(account) = account {
-                auth_context.user.set(Some(Ok(account)));
-                let websocket_context = expect_context::<WebsocketContext>();
-                websocket_context.close();
-                websocket_context.open();
-            } else {
-                auth_context.user.set(None);
-            }
-        });
+        auth_context.action.dispatch(());
     }
 }
