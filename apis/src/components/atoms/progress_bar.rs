f461b41788b0aae693a9f548e3f85c309ddd1472
Penguinwithatie <Penguinwithatie@protonmail.com>
2025-03-03 21:34:10 -0600

    progress bar, update thiserror

--- apis/src/components/atoms/progress_bar.rs
+++ apis/src/components/atoms/progress_bar.rs
@@ -2,12 +2,11 @@ use leptos::prelude::*;
 
 #[component]
 pub fn ProgressBar(current: Signal<usize>, total: usize) -> impl IntoView {
-    let progress = Signal::derive(move || {
-        let progress = current.get() as f64 / total as f64;
-        progress * 100.0
-    });
     let indicator_style =
-        Signal::derive(move || format!("transform: translateX(-{}%)", 100.0 - progress.get()));
+        Signal::derive(move || format!("width: {}%",{
+            let progress = current() as f64 / total as f64;
+            progress * 100.0
+        }));
     view! {
         <Show when=move || { total > 0 }>
             <div class="flex flex-col gap-1 justify-center items-center w-full">
@@ -17,26 +16,9 @@ pub fn ProgressBar(current: Signal<usize>, total: usize) -> impl IntoView {
                     /
                     {total}
                 </div>
-                <div class="w-4/5">
-                    /*<ProgressRoot
-                        attr:class="relative overflow-hidden bg-white rounded-full w-full h-[20px] drop-shadow-md"
-                        attr:style="transform: translateZ(0)"
-                        value=progress
-                    >
-                        <ProgressIndicator
-                            attr:class="bg-orange-twilight w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
-                            attr:style=indicator_style
-                        />
-                    </ProgressRoot>*/
-                
-
-<div class="w-full h-2.5 bg-gray-200 rounded-full dark:bg-gray-700">
-<div class="h-2.5 bg-blue-600 rounded-full" style="width: 45%"></div>
+<div class="w-4/5 h-5 bg-white rounded-full drop-shadow-md">
+<div class="h-5 rounded-full bg-orange-twilight" style=indicator_style></div>
 </div>
-
-
-                    "FIX PROGRESS BAR"
-                </div>
             </div>
         </Show>
     }
