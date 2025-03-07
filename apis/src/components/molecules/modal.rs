1ffacd30c44704b4804d9432bd57c146bea2fc6f
Penguinwithatie <Penguinwithatie@protonmail.com>
2025-03-07 13:27:53 -0600

    custom challenge works consistently

--- apis/src/components/molecules/modal.rs
+++ apis/src/components/molecules/modal.rs
@@ -1,5 +1,5 @@
-use leptos::{ev::MouseEvent, html::Dialog, prelude::*};
-use wasm_bindgen::JsCast;
+use leptos::{html::Dialog, prelude::*};
+use leptos_use::on_click_outside;
 
 #[component]
 pub fn Modal(
@@ -7,49 +7,26 @@ pub fn Modal(
     children: Children,
     dialog_el: NodeRef<Dialog>,
 ) -> impl IntoView {
-    let on_click = move |ev: MouseEvent| {
-        let rect = dialog_el
-            .get_untracked()
-            .expect("dialog to have been created")
-            .get_bounding_client_rect();
-        let click_is_in_dialog = rect.top() <= ev.client_y() as f64
-            && ev.client_y() as f64 <= rect.top() + rect.height()
-            && rect.left() <= ev.client_x() as f64
-            && ev.client_x() as f64 <= rect.left() + rect.width();
-        if !click_is_in_dialog {
-            ev.target()
-                .expect("Event target")
-                .unchecked_into::<web_sys::HtmlDialogElement>()
-                .close();
-            open.set(false);
+    let close_dialog = move || {
+        if let Some(dialog_el) = dialog_el.get() { 
+            dialog_el.close();
+            open.set(false); 
         }
     };
-
-    Effect::new(move |_| {
-        if let Some(dialog) = dialog_el.get_untracked() {
-            if open() {
-                if dialog.show_modal().is_err() {
-                    dialog.set_open(true);
-                }
-            } else {
-                dialog.close();
-            }
-        }
-    });
-
+    #[allow(unused)]
+    on_click_outside(dialog_el, move |_| close_dialog());
     view! {
         <dialog
             node_ref=dialog_el
-            open=open.get_untracked()
+            open=open
             class="rounded-lg border shadow-xl drop-shadow-xl backdrop:backdrop-blur bg-stone-300 dark:bg-gray-600 dark:border-gray-500"
-            // clicking on ::backdrop should dismiss modal
-            on:click=on_click
         >
             <header class="flex justify-end">
                 <form class="m-2" method="dialog">
                     <button
                         class="flex justify-center items-center w-5 h-5 rounded-full duration-300 hover:bg-ladybug-red active:scale-95"
                         aria-label="Close"
+                        on:click= move |_| close_dialog()
                     >
                         x
                     </button>
