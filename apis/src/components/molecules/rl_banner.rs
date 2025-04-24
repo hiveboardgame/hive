b2b6c51611b6b2c6f9340dfec7cb2d35f3bfbc00
PenguinWithATie <166940857+PenguinWithATie@users.noreply.github.com>
2025-04-24 12:20:51 -0600

    fix markdown html (#488)

--- apis/src/components/molecules/rl_banner.rs
+++ apis/src/components/molecules/rl_banner.rs
@@ -22,7 +22,7 @@ pub fn RlBanner(title: String, content: String) -> impl IntoView {
     view! {
         <div class="flex flex-col justify-center items-center p-6 mb-3 text-black rounded-sm bg-orange-twilight xs:p-8 xs:mb-4">
             <h1 class="flex items-center mb-4 text-2xl font-bold xs:text-4xl">{title}</h1>
-            <div class="prose dark:prose-invert" inner_html=markdown_desc />
+            <div class="text-black prose" inner_html=markdown_desc />
         </div>
     }
 }
