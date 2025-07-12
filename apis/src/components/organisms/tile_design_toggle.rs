44e8bdae95a6a0becaedbe8ecc8f7e7da1e9eab2
leex <leex@klautcomputing.de>
2025-07-12 11:19:05 +0200

    Makes all tiles available in all browsers

--- apis/src/components/organisms/tile_design_toggle.rs
+++ apis/src/components/organisms/tile_design_toggle.rs
@@ -1,39 +1,26 @@
 use crate::i18n::*;
 use crate::{common::TileDesign, providers::Config};
-use lazy_static::lazy_static;
 use leptos::prelude::*;
-use leptos_use::use_window;
-
-lazy_static! {
-    pub static ref NOT_APPLE: bool =
-        if let Some(Ok(user_agent)) = use_window().navigator().map(|n| n.user_agent()) {
-            !(user_agent.contains("Safari")
-                || user_agent.contains("iPhone")
-                || user_agent.contains("iPad")
-                || user_agent.contains("iPod"))
-        } else {
-            true
-        };
-}
 
 #[component]
 pub fn TileDesignToggle() -> impl IntoView {
     let i18n = use_i18n();
-    let good_software = RwSignal::new(false);
-    Effect::new(move |_| good_software.update(|b| *b = *NOT_APPLE));
     view! {
         <p class="m-1 text-black dark:text-white">{t!(i18n, user_config.piece_style)}</p>
+        <div class="mb-2 p-2 bg-yellow-50 dark:bg-yellow-900 border border-yellow-200 dark:border-yellow-700 rounded-md">
+            <p class="text-sm text-yellow-800 dark:text-yellow-200">
+                "⚠️ Note: Not all browsers support all tile designs. For the best experience, we recommend using Firefox."
+            </p>
+        </div>
         <div class="flex flex-wrap">
             <TileDesignButton tile_design=TileDesign::Official />
             <TileDesignButton tile_design=TileDesign::Flat />
-            <Show when=good_software>
-                <TileDesignButton tile_design=TileDesign::ThreeD />
-                <TileDesignButton tile_design=TileDesign::Carbon3D />
-            </Show>
             <TileDesignButton tile_design=TileDesign::HighContrast />
+            <TileDesignButton tile_design=TileDesign::ThreeD />
             <TileDesignButton tile_design=TileDesign::Community />
             <TileDesignButton tile_design=TileDesign::Pride />
             <TileDesignButton tile_design=TileDesign::Carbon />
+            <TileDesignButton tile_design=TileDesign::Carbon3D />
         </div>
     }
 }
