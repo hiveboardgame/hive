35c30efbfd562c4a0a8699e042adc1ab14d9f582
ioni <104294646+IongIer@users.noreply.github.com>
2025-07-19 12:29:23 +0300

    Fixes notifications not showing up

--- apis/src/websocket/client_handlers/schedule/handler.rs
+++ apis/src/websocket/client_handlers/schedule/handler.rs
@@ -1,77 +1,67 @@
-use std::collections::HashMap;
-
 use crate::{
     common::ScheduleUpdate::{self, *},
     providers::{schedules::SchedulesContext, AuthContext, NotificationContext},
+    responses::ScheduleResponse,
 };
-use leptos::prelude::{expect_context, Get, Set, Update};
+use leptos::prelude::{expect_context, Get, RwSignal, Set, Update};
+use shared_types::GameId;
+use std::collections::HashMap;
+use uuid::Uuid;
 
 pub fn handle_schedule(schedule_update: ScheduleUpdate) {
     let ctx = expect_context::<SchedulesContext>();
     let notifications = expect_context::<NotificationContext>();
     let auth_context = expect_context::<AuthContext>();
 
+    let update_schedules =
+        |signal: RwSignal<_>, response: &ScheduleResponse, reset_agreed: bool| {
+            signal.update(|h: &mut HashMap<GameId, HashMap<Uuid, ScheduleResponse>>| {
+                let schedules = h.entry(response.game_id.clone()).or_default();
+                if reset_agreed {
+                    for (_, schedule) in schedules.iter_mut() {
+                        schedule.agreed = false;
+                    }
+                }
+                schedules.insert(response.id, response.clone());
+            });
+        };
+
     match schedule_update {
         Accepted(response) => {
-            notifications.schedule_proposals.update(|proposals| {
-                proposals.remove(&response.id);
+            notifications.schedule_proposals.update(|p| {
+                p.remove(&response.id);
             });
 
-            // Add acceptance notification for the proposer
             if let Some(user) = auth_context.user.get() {
                 if response.proposer_id == user.id {
-                    notifications.schedule_acceptances.update(|acceptances| {
-                        acceptances.insert(response.id);
+                    notifications.schedule_acceptances.update(|a| {
+                        a.insert(response.id);
                     });
                 }
             }
 
-            ctx.tournament.update(|h| {
-                if let Some(schedules) = h.get_mut(&response.game_id) {
-                    for (_, schedule) in schedules.iter_mut() {
-                        schedule.agreed = false;
-                    }
-                    schedules.insert(response.id, response.clone());
-                } else {
-                    let mut new_schedules = HashMap::new();
-                    new_schedules.insert(response.id, response.clone());
-                    h.insert(response.game_id.clone(), new_schedules);
-                }
-            });
-            ctx.own.update(|h| {
-                if let Some(schedules) = h.get_mut(&response.game_id) {
-                    for (_, schedule) in schedules.iter_mut() {
-                        schedule.agreed = false;
-                    }
-                    schedules.insert(response.id, response);
-                }
-            });
+            update_schedules(ctx.tournament, &response, true);
+            update_schedules(ctx.own, &response, true);
         }
+
         Proposed(response) => {
             if let Some(user) = auth_context.user.get() {
                 if response.opponent_id == user.id {
-                    notifications.schedule_proposals.update(|proposals| {
-                        proposals.insert(response.id);
+                    notifications.schedule_proposals.update(|p| {
+                        p.insert(response.id);
                     });
                 }
             }
 
-            ctx.own.update(|h| {
-                if let Some(schedules) = h.get_mut(&response.game_id) {
-                    schedules.insert(response.id, response);
-                } else {
-                    let mut new_schedules = HashMap::new();
-                    new_schedules.insert(response.id, response.clone());
-                    h.insert(response.game_id, new_schedules);
-                }
-            });
+            update_schedules(ctx.own, &response, false);
         }
+
         Deleted(response) => {
-            notifications.schedule_proposals.update(|proposals| {
-                proposals.remove(&response.id);
+            notifications.schedule_proposals.update(|p| {
+                p.remove(&response.id);
             });
-            notifications.schedule_acceptances.update(|proposals| {
-                proposals.remove(&response.id);
+            notifications.schedule_acceptances.update(|a| {
+                a.remove(&response.id);
             });
 
             ctx.tournament.update(|h| {
@@ -85,6 +75,7 @@ pub fn handle_schedule(schedule_update: ScheduleUpdate) {
                 }
             });
         }
+
         TournamentSchedules(schedules) => ctx.tournament.set(schedules),
         OwnTournamentSchedules(schedules) => ctx.own.set(schedules),
     }
