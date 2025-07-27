c25ac416f93e7469170661612039b6fbebcd22bf
leex <leex@klautcomputing.de>
2025-07-27 11:17:13 +0200

    Adds game stats job

--- apis/src/jobs/mod.rs
+++ apis/src/jobs/mod.rs
@@ -1,10 +1,12 @@
 pub mod challenge_cleanup;
 pub mod game_cleanup;
+pub mod game_stats;
 pub mod heartbeat;
 pub mod ping;
 pub mod tournament_start;
 pub use challenge_cleanup::run as challenge_cleanup;
 pub use game_cleanup::run as game_cleanup;
+pub use game_stats::run_once as game_stats;
 pub use heartbeat::run as heartbeat;
 pub use ping::run as ping;
 pub use tournament_start::run as tournament_start;
