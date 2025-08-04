0f0c367658f6ff4319da33a4d8fc46563ac6dc96
ioni <104294646+IongIer@users.noreply.github.com>
2025-08-04 18:25:16 +0300

    Raises connecting above other elements

--- apis/src/components/layouts/base_layout.rs
+++ apis/src/components/layouts/base_layout.rs
@@ -181,7 +181,7 @@ pub fn BaseLayout(children: ChildrenFn) -> impl IntoView {
             <Alert />
             <TournamentReadyPopup ready_signal=update_notifier.tournament_ready />
             <Show when=move || ws_ready() != ConnectionReadyState::Open>
-                <div class="flex absolute top-1/2 left-1/2 gap-2 items-center transform -translate-x-1/2 -translate-y-1/2">
+                <div class="flex absolute top-1/2 left-1/2 gap-2 items-center transform -translate-x-1/2 -translate-y-1/2 z-[60]">
                     <div class="w-10 h-10 rounded-full border-t-2 border-b-2 border-blue-500 animate-spin"></div>
                     <div class="text-lg font-bold text-ladybug-red">Connecting..</div>
                 </div>
