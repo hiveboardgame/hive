8404e6236bdf22ef0f205180780828cd187d72cf
ioni <104294646+IongIer@users.noreply.github.com>
2025-12-09 12:00:25 +0200

    Fixes create challenge button visual bug

--- apis/src/components/atoms/create_challenge_button.rs
+++ apis/src/components/atoms/create_challenge_button.rs
@@ -9,46 +9,23 @@ pub fn CreateChallengeButton(
     create_challenge: Callback<ColorChoice>,
 ) -> impl IntoView {
     let config = expect_context::<Config>().0;
-    let icon_data = move |color_choice: ColorChoice| {
-        config.with(|cfg| match color_choice {
-            ColorChoice::Random => (icondata_bs::BsHexagonHalf, "size-full"),
-            ColorChoice::White => {
-                if cfg.prefers_dark {
-                    (icondata_bs::BsHexagonFill, "size-full fill-white")
-                } else {
-                    (
-                        icondata_bs::BsHexagon,
-                        "size-full stroke-1 stroke-black",
-                    )
-                }
-            }
-            ColorChoice::Black => {
-                if cfg.prefers_dark {
-                    (
-                        icondata_bs::BsHexagon,
-                        "size-full stroke-1 stroke-white",
-                    )
-                } else {
-                    (icondata_bs::BsHexagonFill, "size-full fill-black")
-                }
-            }
+    let icon = Signal::derive(move || {
+        config.with(|cfg| match (color_choice.get_value(), cfg.prefers_dark) {
+            (ColorChoice::Random, _) => icondata_bs::BsHexagonHalf,
+            (ColorChoice::White, false) | (ColorChoice::Black, true) => icondata_bs::BsHexagon,
+            _ => icondata_bs::BsHexagonFill,
         })
-    };
-
+    });
     view! {
         <button
             title=color_choice.get_value().to_string()
             formmethod="dialog"
-            class=format!(
-                "m-1 h-[4.5rem] w-16 bg-odd-light dark:bg-gray-700 my-1 p-1 transform transition-transform duration-300 active:scale-95 hover:shadow-xl dark:hover:shadow dark:hover:shadow-gray-500 drop-shadow-lg dark:shadow-gray-600 rounded",
-            )
+            type="submit"
+            class="m-1 h-[4.5rem] w-16 bg-odd-light dark:bg-gray-700 my-1 p-1 transform transition-transform duration-300 active:scale-95 hover:shadow-xl dark:hover:shadow dark:hover:shadow-gray-500 drop-shadow-lg dark:shadow-gray-600 rounded"
 
             on:click=move |_| { create_challenge.run(color_choice.get_value()) }
         >
-            {move || {
-                let (icon, class) = icon_data(color_choice.get_value());
-                view! { <Icon icon attr:class=class /> }
-            }}
+            <Icon icon style="height:100%; width:100%" />
         </button>
     }
 }
