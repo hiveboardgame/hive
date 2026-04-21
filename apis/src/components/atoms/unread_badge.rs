fa32ac78bb2653313f454129c090f5102c3b1600
ioni <104294646+IongIer@users.noreply.github.com>
2026-04-21 15:11:31 +0300

    Centers div

--- apis/src/components/atoms/unread_badge.rs
+++ apis/src/components/atoms/unread_badge.rs
@@ -1,8 +1,8 @@
 use leptos::prelude::*;
 
 const UNREAD_BADGE_BASE_CLASS: &str =
-    "shrink-0 inline-flex items-center justify-center min-w-5 h-5 px-1.5 \
-     text-xs font-medium leading-none text-white rounded-full \
+    "shrink-0 inline-grid place-items-center min-w-5 h-5 px-1.5 \
+     text-center text-xs font-medium tabular-nums leading-none text-white rounded-full \
      bg-ladybug-red dark:bg-red-500";
 
 fn format_unread_count(count: i64) -> String {
@@ -19,7 +19,9 @@ pub fn UnreadBadge(count: Signal<i64>) -> impl IntoView {
 
     view! {
         <Show when=move || unread_count().gt(&0)>
-            <span class=UNREAD_BADGE_BASE_CLASS>{move || format_unread_count(unread_count())}</span>
+            <span class=UNREAD_BADGE_BASE_CLASS>
+                <span class="relative top-px">{move || format_unread_count(unread_count())}</span>
+            </span>
         </Show>
     }
 }
